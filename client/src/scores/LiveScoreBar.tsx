cfd6f1f892c014e43d4ec96d37c2f268f2411888
rosemit <Mitchell.Rose@mpac.ca>
2022-10-11 20:59:08 -0400

    created skeleton for Live Score Bar

---
+++ client/src/scores/LiveScoreBar.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import Container from '@mui/material/Container';
+
+const LiveScoreBar = () => {
+    
+    return (
+        <Container maxWidth={false} sx={{minWidth: '100%', border: '1px solid red'}}>
+            <h1>Mitch</h1>
+        </Container>
+    )
+};
+
+export default LiveScoreBar;
