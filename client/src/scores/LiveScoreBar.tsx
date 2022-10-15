8ace11c9b8bba986f564c69282a5560a41c69d46
rosemit <Mitchell.Rose@mpac.ca>
2022-10-14 20:17:34 -0400

    added card component for individual game info

--- client/src/scores/LiveScoreBar.tsx
+++ client/src/scores/LiveScoreBar.tsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from "react";
 import Container from '@mui/material/Container';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
 
 const LiveScoreBar = () => {
     
     return (
         <Container maxWidth={false} sx={{minWidth: '100%', border: '1px solid red'}}>
-            <h1>Mitch</h1>
+            
         </Container>
     )
 };
